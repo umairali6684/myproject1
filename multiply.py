81f5d6aa27380071bd5e60be106905857bdcfe8e
umairali6684 <umairali6684@gmail.com>
2023-03-22 00:11:42 +0530

    added multiply.py

---
+++ multiply.py
@@ -0,0 +1,9 @@
+# get user input
+number1 = int(input("Enter first number: "))
+number2 = int(input("Enter second number: "))
+
+# multiply the numbers
+result = number1 * number2
+
+# display the result
+print("The product of", number1, "and", number2, "is", result)
